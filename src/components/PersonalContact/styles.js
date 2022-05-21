26213f9d2f3cccda9df9b9ecb131833d89199259
Flaviane-Brum <flavianebs3@gmail.com>
2022-05-21 14:00:12 -0300

    :sparkles:  Add style of the PersonalContact  component

---
+++ src/components/PersonalContact/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const PersonalContact = styled.div`
+  padding: 1rem;
+  box-shadow: 0 1px 2px 1px rgb(0 0 0 / 30%);
+  flex: 1;
+  background-color: var(--white);
+  border-radius: 5px;
+`;
