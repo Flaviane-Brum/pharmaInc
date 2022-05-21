6c7b60e0f42a0d2fbc871150ed83aa3a70e8a6d1
Flaviane-Brum <flavianebs3@gmail.com>
2022-05-21 14:08:18 -0300

    :sparkles: Add style of the SearchNat  component

---
+++ src/components/SearchNat/styles.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const FormIput = styled.div`
+  height: 40px;
+  width: 100%;
+  display: flex;
+  position: relative;
+  border-radius: 5px;
+  input {
+    flex: 1;
+    padding: 1rem;
+    border: 0;
+    border-radius: 5px;
+    font: var(--font-s);
+    font-weight: 700;
+    color: var(--gray-paragraph);
+    border: 1px solid #ccc;
+    &:focus {
+      outline: none;
+      box-shadow: 0 0 0 1px #c0fff5;
+      border-color: #212529;
+    }
+    &::placeholder {
+      color: #ccc;
+    }
+  }
+  svg {
+    position: absolute;
+    border: 1px solid transparent;
+    background-color: var(--white);
+    right: 10px;
+    top: 1px;
+    height: 32px;
+    border-radius: 0 5px 5px 0;
+    &:focus {
+      outline: none;
+      border-color: transparent;
+    }
+  }
+`;
